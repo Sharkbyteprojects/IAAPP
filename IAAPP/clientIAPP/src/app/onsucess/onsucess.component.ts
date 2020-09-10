c30dd4b66c332043f8f0eb5453a549ffaed7729a
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-09-10 11:10:22 +0200

    ADD SECRETS

--- IAAPP/clientIAPP/src/app/onsucess/onsucess.component.ts
+++ IAAPP/clientIAPP/src/app/onsucess/onsucess.component.ts
@@ -9,8 +9,21 @@ export class OnsucessComponent implements OnInit {
   @Input() xrs;
   hardness:string="";
   constructor() { }
-
+  topsecret="";
   ngOnInit(): void {
+	const avail:string[]=["MY SECRET IS A RANDOM THING",
+						  "MY LEMONCAKE HAS 3 THING\n\nBUTTER\nLEMONS\nEGGS",
+						  "I HATE TRASH",
+						  "HAVE YOU EATEN MY CAR?",
+						  "WHAT IS HERE?",
+						  "THIS APP USE ANGULAR",
+						  "THIS APP USE NODE.JS",
+						  ":))",
+						  "////MYFILE////",
+						  "Samba is insecure",
+						  "eval is evil",
+						  "I LIKE SOME CATS"];
+	this.topsecret = `TOP SECRET:\n\n${avail[Math.round(Math.random()*(avail.length-1))]}`;
     this.hardness=this.xrs;
   }
 
